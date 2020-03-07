682435c5030b29589ab4b8bed5b326913e8a3e9b
LordDamionDevil <robloxfilmcam@gmail.com>
2020-03-08 00:12:35 +0100

    Update getsong.php

--- api/getsong.php
+++ api/getsong.php
@@ -56,6 +56,17 @@ if($station == "TRAP.FM"){
     $tna = $obj['artist']." - ".$obj['title'];
     $output['title'] = $tna;
     echo json_encode($output, JSON_PRETTY_PRINT);
+}elseif($station == "ILoveMashup"){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL, 'https://www.ilovemusic.de/typo3conf/ext/ep_channel/Scripts/playlist.php');
+    $result = curl_exec($ch);
+    curl_close($ch);
+    $obj = json_decode($result, true);
+    $tna = $obj['channel-5']['artist']." - ".$obj['channel-5']['title'];
+    $output['title'] = $tna;
+    echo json_encode($output, JSON_PRETTY_PRINT);
 }else{
     $output['title'] = $station;
     echo json_encode($output, JSON_PRETTY_PRINT);
