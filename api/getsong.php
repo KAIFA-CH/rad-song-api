0e703ac1067dbaac85e96c521aa854a53611c241
LordDamionDevil <robloxfilmcam@gmail.com>
2020-03-11 02:22:42 +0100

    Update getsong.php

--- api/getsong.php
+++ api/getsong.php
@@ -119,15 +119,7 @@ if($station === "TRAP.FM"){
 	$output['title'] = $obj['Header']['Subtitle'];
     echo json_encode($output, JSON_PRETTY_PRINT);
 }elseif($station === "101.1BigFM"){
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_URL, 'https://globalnewselection.s3.amazonaws.com/fm-playlist/results/CIQBFM_np.js');
-    $result = curl_exec($ch);
-    curl_close($ch);
-    $obj = json_decode($result, true);
-	$tna = $obj['artist_name']." - ".$obj['song_name'];
-	$output['title'] = $tna;
+	$output['title'] = "N/A";
     echo json_encode($output, JSON_PRETTY_PRINT);
 }else{
     $output['title'] = $station;
