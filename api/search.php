dc8577d7243d3827a53a4e9295c4e29a1286833c
LordDamionDevil <robloxfilmcam@gmail.com>
2020-03-17 02:10:47 +0100

    Fix search.php and now.json

--- api/search.php
+++ api/search.php
@@ -1,7 +1,7 @@
 <?php
-$conn = new mysqli("mysql://ip469qaanzsm59el:hs1z4zwo5k0gs5qp@olxl65dqfuqr6s4y.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/gnq5x7yjtzi40ngj");
+$conn = new mysqli("olxl65dqfuqr6s4y.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "ip469qaanzsm59el", "hs1z4zwo5k0gs5qp" , "gnq5x7yjtzi40ngj");
 $input = $_GET['input'];
-if(empty(input)){
+if(empty($input)){
     echo "empty";
 }else{
     echo "input found";
