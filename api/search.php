9f9792b5a77429d6f67f60d2fec29ba83fa08486
LordDamionDevil <robloxfilmcam@gmail.com>
2020-03-17 02:19:31 +0100

    Update search.php

--- api/search.php
+++ api/search.php
@@ -3,6 +3,20 @@ $conn = new mysqli("olxl65dqfuqr6s4y.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", 
 $input = $_GET['input'];
 if(empty($input)){
     echo "empty";
+    $sql = "SELECT * FROM radios";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            echo "img url: " . $row["img"] . " - Name: " . $row["name"] . " - Desc: " . $row["desc"] . "<br>";
+        }
+    }
 }else{
     echo "input found";
+    $sql = "SELECT * FROM radios WHERE name=".$input;
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            echo "img url: " . $row["img"] . " - Name: " . $row["name"] . " - Desc: " . $row["desc"] . "<br>";
+        }
+    }
 }
