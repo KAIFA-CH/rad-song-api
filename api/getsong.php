9f565746eebaa5d2156b8e081a86a411f65ab70a
LordDamionDevil <robloxfilmcam@gmail.com>
2020-06-15 04:05:43 +0200

    Update getsong.php

--- api/getsong.php
+++ api/getsong.php
@@ -287,7 +287,7 @@ if($station === "TRAP.FM"){
     $result = curl_exec($ch);
     curl_close($ch);
     $obj = json_decode($result, true);
-	$output['title'] = $obj[0]['playlist'][0]['song'] // + ' - ' + $obj[0]['playlist'][0]['song']['title'];
+	$output['title'] = $obj[0]['playlist'][0]['song']; // + ' - ' + $obj[0]['playlist'][0]['song']['title'];
     echo json_encode($output, JSON_PRETTY_PRINT);
 }else{
     $output['title'] = "No Data Available";
